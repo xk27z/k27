aeeed3a64c637f0960b27ba22f8f0c36b3ec700e
xk27z <137503567+xk27z@users.noreply.github.com>
2024-05-24 12:35:59 +0300

    Add files via upload

---
+++ js/script3.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".multiple-text", {
+    strings: [ "Travel with k27..", "This is a Travels Blog.."],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
+
